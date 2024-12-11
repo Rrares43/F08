b187e17da0d105d9b4d3b341419b8b8959d9e9c4
RARES AURELIAN RADUCANU <raducanurares40@gmail.com>
2024-12-11 13:48:17 +0200

    Update Teste

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -4,6 +4,7 @@ import employee
 @pytest.fixture(autouse=True)
 def reset():
     employee.Employee.empCount = 0 #asta am facut ca sa mi se reseteze countul la fiecare testare
+    employee.Manager.mgrCount = 0
 
 def test_display_employee():
     emp = employee.Manager("Andrei", "3500", "Vanzare")
@@ -15,9 +16,16 @@ def test_display_employee():
     assert emp2.name == "Super Mario"
     assert emp2.salary == "10000"
     assert emp2._department == "F08_Instalatii"
-    
+
 def test_emp_count():
-    emp1 = employee.Manager("Dana", "4900", "Stocare")
-    empdoi = employee.Manager("Erd", "0", "Caritate")
-    assert employee.Employee.empCount == 2
+    emp1 = employee.Employee("Dana", "4900")
+    empdoi = employee.Employee("Erd", "4500")
+    emptrei = employee.Employee("Gigel", "5400")
+    assert employee.Employee.empCount == 3
+    
+    
+def test_mgr_count():
+    emp1 = employee.Manager("Shana", "4300", "Stocare")
+    empdoi = employee.Manager("Radu", "4000", "Comenzi")
+    assert employee.Manager.mgrCount == 2
     
